d22b03b5308d0060f2e9cc40ba163d9e79d12716
mlm0207 <54418596+mlm0207@users.noreply.github.com>
2021-10-03 16:13:21 -0400

    Delete HotDog_Stand_App directory

--- HotDog_Stand_App/HotDogStandApp.py
+++
@@ -1,9 +0,0 @@
-from HotDogStand import HotDogStand
-
-def main():
-    print("Welcome to Talon's HotDog Stand!")
-    hds = HotDogStand()
-    hds.displayActions()
-
-if __name__ == "__main__":
-    main()
